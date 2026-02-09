8105fc11e3e06ef86df455da4d9b156a7774d3a4
psxbox <adhamyu83@gmail.com>
2026-02-09 09:57:07 +0500

    Changed aspire integrations

--- src/MyIOT.AppHost/AppHost.cs
+++ src/MyIOT.AppHost/AppHost.cs
@@ -1,3 +1,21 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddDockerComposeEnvironment("env");
+
+var redis = builder.AddRedis("redis");
+
+var timescaledb = builder.AddPostgres("timescaledb")
+    .WithImage("timescale/timescaledb:latest-pg16")
+    .WithDataVolume("pgdata")
+    .WithEnvironment("POSTGRES_USER", "postgres")
+    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
+    .WithEnvironment("POSTGRES_DB", "myiot");
+
+builder.AddProject<MyIOT_Api>("api")
+    .WithHttpHealthCheck()
+    .WaitFor(timescaledb)
+    .WaitFor(redis);
 
 builder.Build().Run();
